6ba1486a6872645a9e64b7bd18de20d52a739651
kkarapetyan2012 <kkarapetyan2012@gmail.com>
2023-04-28 23:20:42 +0400

    new install

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -46,6 +46,10 @@ const optionsAge = [
   { value: '38', label: '38' },
   { value: '39', label: '39' },
   { value: '40', label: '40' },
+  { value: '41', label: '41' },
+  { value: '42', label: '42' },
+  { value: '43', label: '43' },
+  { value: '44', label: '44' },
 ];
 
 const optionsLocation = [
